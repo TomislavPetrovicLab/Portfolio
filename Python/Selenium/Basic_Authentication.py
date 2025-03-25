d22a076ca086c11acda6f4b69a60f5f753fe6dae
tomlab <125453365+TomislavPetrovicLab@users.noreply.github.com>
2025-03-25 11:41:29 +0100

    Replace time.sleep() with implicit wait for dynamic element loading

--- Python/Selenium/Basic_Authentication.py
+++ Python/Selenium/Basic_Authentication.py
@@ -9,7 +9,9 @@ url = f"https://{username}:{password}@the-internet.herokuapp.com/basic_auth"
 browser = webdriver.Chrome()
 browser.maximize_window()
 
-time.sleep(4)
+# Use implicit wait instead of time.sleep()
+browser.implicitly_wait(5)  # Waits up to 5 seconds for elements to be found
+
 browser.get(url)
 
 time.sleep(2)
